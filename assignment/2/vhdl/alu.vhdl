0289bef5b389d5a8de25c96337e432a792b68233
fricai <rishabh.dhiman.145@gmail.com>
2022-02-09 21:11:37 +0530

    Redesigned ALU

--- assignment/2/vhdl/alu.vhdl
+++ assignment/2/vhdl/alu.vhdl
@@ -7,31 +7,33 @@ use work.MyTypes.all;
 entity alu is
 	port(
 	op1, op2: in word;
-	res: out word;
+	result: out word;
 	instr: in optype;
 	carry_in: in std_logic;
 	carry_out: out std_logic);
 end alu;
 
 architecture beh of alu is
-	constant one: word := word(to_unsigned(1, 32));
+	constant one: unsigned := to_unsigned(1, 32);
+	signal tmp: unsigned(32 downto 0);
 begin
 	with instr select
-		res <= op1 and op2 when andop, -- 0000
-		       op1 xor op2 when eor, -- 0001
-		       word(unsigned(op1) + unsigned(not op2) + unsigned(one)) when sub, -- 0010
-		       word(unsigned(not op1) + unsigned(op2) + unsigned(one)) when rsb, -- 0011
-		       word(unsigned(op1) + unsigned(op2)) when add, -- 0100
-		       word(unsigned(op1) + unsigned(op2) + unsigned'("" & carry_in)) when adc, -- 0101
-		       word(unsigned(op1) + unsigned(not op2) + unsigned'("" & carry_in)) when sbc, -- 0110
-		       word(unsigned(not op1) + unsigned(op2) + unsigned'("" & carry_in)) when rsc, -- 0111
-		       op1 and op2 when tst, -- 1000
-		       op1 xor op2 when teq, -- 1001
-		       word(unsigned(op1) + unsigned(not op2) + unsigned(one)) when cmp, -- 1010
-		       word(unsigned(op1) + unsigned(op2)) when cmn, -- 1011
-		       op1 or op2 when orr, -- 1100
-		       op2 when mov, -- 1101
-		       op1 and (not op2) when bic, -- 1110
-		       not op2 when mvn; -- 1111
-	-- need to define how carry_out is affected
+		tmp <= unsigned(op1 and op2) when andop, -- 0000
+		       unsigned(op1 xor op2) when eor, -- 0001
+		       unsigned(op1) + unsigned(not op2) + one when sub, -- 0010
+		       unsigned(not op1) + unsigned(op2) + one when rsb, -- 0011
+		       unsigned(op1) + unsigned(op2) when add, -- 0100
+		       unsigned(op1) + unsigned(op2) + unsigned'("" & carry_in) when adc, -- 0101
+		       unsigned(op1) + unsigned(not op2) + unsigned'("" & carry_in) when sbc, -- 0110
+		       unsigned(not op1) + unsigned(op2) + unsigned'("" & carry_in) when rsc, -- 0111
+		       unsigned(op1 and op2) when tst, -- 1000
+		       unsigned(op1 xor op2) when teq, -- 1001
+		       unsigned(op1) + unsigned(not op2) + one when cmp, -- 1010
+		       unsigned(op1) + unsigned(op2) when cmn, -- 1011
+		       unsigned(op1 or op2) when orr, -- 1100
+		       unsigned(op2) when mov, -- 1101
+		       unsigned(op1) and unsigned(not op2) when bic, -- 1110
+		       unsigned(not op2) when mvn; -- 1111
+	result <= std_logic_vector(tmp(31 downto 0));
+	carry_out <= tmp(32);
 end beh;
