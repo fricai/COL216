3d032ebffdf1b7f0ed46b84d12a61f9abf7bb4c3
fricai <rishabh.dhiman.145@gmail.com>
2022-02-19 19:42:30 +0530

    Finished coding stage 2

--- assignment/2/vhdl/src/decoder.vhdl
+++ assignment/2/vhdl/src/decoder.vhdl
@@ -8,25 +8,26 @@ use work.MyTypes.all;
 
 entity decoder is
 	Port (
-		     instruction : in word;
-		     instr_class : out instr_class_type;
-		     operation : out optype;
-		     DP_subclass : out DP_subclass_type;
-		     DP_operand_src : out DP_operand_src_type;
-		     load_store : out load_store_type;
-		     DT_offset_sign : out DT_offset_sign_type
+		     instruction: in word;
+		     instr_class: out instr_class_type;
+		     operation: out optype;
+		     DP_subclass: out DP_subclass_type;
+		     DP_operand_src: out DP_operand_src_type;
+		     load_store: out load_store_type;
+		     DT_offset_sign: out DT_offset_sign_type;
 		     cond: out condtype;
+		     S_bit: out std_logic
 	     );
 end decoder;
 
 architecture behavioral of decoder is
 	type oparraytype is array (0 to 15) of optype;
-	constant oparray : oparraytype := (andop, eor, sub, rsb, add, adc, sbc, rsc, tst, teq, cmp, cmn, orr, mov, bic, mvn);
+	constant oparray: oparraytype := (andop, eor, sub, rsb, add, adc, sbc, rsc, tst, teq, cmp, cmn, orr, mov, bic, mvn);
 
 	type condarraytype is array (0 to 14) of condtype;
-	constant condarray : condarraytype := (eq, ne, cs, cc, mi, pl, vs, vc, hi, ls, ge, lt, gt, le, al);
+	constant condarray: condarraytype := (eq, ne, cs, cc, mi, pl, vs, vc, hi, ls, ge, lt, gt, le, al);
 begin
-	with instruction (27 downto 26) select
+	with instruction(27 downto 26) select
 		instr_class <= DP when "00",
 			       DT when "01",
 			       BRN when "10",
@@ -43,5 +44,8 @@ begin
 	load_store <= load when instruction(20) = '1' else store;
 	DT_offset_sign <= plus when instruction(23) = '1' else minus;
 
-	cond <= condarray(to_integer(unsigned(instruction(31 downto 28)))); -- my addition
+	-- my addition
+	cond <= condarray(to_integer(unsigned(instruction(31 downto 28))));
+
+	S_bit <= instruction(20);
 end behavioral;
