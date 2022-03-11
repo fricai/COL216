5ff2672ab0ed261a66f5aad0cb75b9a052b74069
fricai <rishabh.dhiman.145@gmail.com>
2022-03-12 01:42:51 +0530

    No more fixes for this stage i think

--- assignment/2/code/arm/assembly/neg-fib.s
+++ assignment/2/code/arm/assembly/neg-fib.s
@@ -10,28 +10,32 @@ mov r1, #1
 str r1, [r0, #4]
 @ set a[0] = 0, a[1] = 1
 
-mov r1, #8
+mov r1, #7
 @ ie, 
 @ a[0] = 0
 @ a[1] = 1
 @ for (i = 1; i <= 8; ++i)
 @   a[i + 1] = a[i - 1] - a[i]
 
-add r0, #4
-mov r2, #1 @ index variable
+mov r2, #0 @ index variable
 loop:
-@ r3 = a[r0 - 1]
-@ r4 = a[r0]
-@ r4 -= r3
-@ a[r0 + 1] = r4
+@ r3 = a[i]
+@ ++i
+@ r4 = a[i]
+@ r3 -= r4
+@ ++i
+@ a[i] = r4
 
-ldr r3, [r0, #-4]
-ldr r4, [r0]
+ldr r3, [r0, r2, LSL #2]
+add r2, #1
+ldr r4, [r0, r2, LSL #2]
 sub r3, r4
-str r3, [r0, #4]
+add r2, #1
+str r3, [r0, r2, LSL #2]
 
 @ update things
-add r0, #4
-add r2, #1
+@ add r0, #4
+@ don't update r0
+sub r2, #1
 cmp r2, r1
 bne loop
